5de89cfe6395a488e083e9576e84cc2c79f7382b
Chi Nguyen <minh_chi.nguyen@mercedes-benz.com>
2026-04-02 11:49:45 +0200

    refactor: DocumentDownloadDialog

--- frontend/libs/portal/components/dialog/DocumentDownloadDialog.vue
+++ frontend/libs/portal/components/dialog/DocumentDownloadDialog.vue
@@ -7,29 +7,27 @@ import ProjectService, {DocumentDownloadVersion} from '@disclosure-portal/servic
 import {useProjectStore} from '@disclosure-portal/stores/project.store';
 import DCActionButton from '@shared/components/disco/DCActionButton.vue';
 import useSnackbar from '@shared/composables/useSnackbar';
-import {ref} from 'vue';
+import {computed, ref} from 'vue';
 import {useI18n} from 'vue-i18n';
 
 const {info: snack} = useSnackbar();
-const show = ref(false);
-const projectKey = ref('');
-const projectName = ref('');
-const appId = ref('');
-const form = ref(null);
 const {t} = useI18n();
 const projectStore = useProjectStore();
 
+const currentProject = computed(() => projectStore.currentProject!);
+
+const show = ref(false);
+const appId = ref('');
+
 const open = (approvalGuid: string) => {
   show.value = true;
-  projectKey.value = projectStore.currentProject!._key;
-  projectName.value = projectStore.currentProject!.name;
   appId.value = approvalGuid;
 };
 
 const close = async () => {
   show.value = false;
-  if (!projectStore.currentProject?.isGroup) {
-    await projectStore.fetchProjectByKey(projectKey.value);
+  if (!currentProject.value.isGroup) {
+    await projectStore.fetchProjectByKey(currentProject.value._key);
   }
 };
 
@@ -38,9 +36,15 @@ const downloadDiscoDocument = (lang: string) => {
   link.click();
   link.target = '_blank';
   link.rel = 'noopener noreferrer';
-  ProjectService.downloadDocumentByTask(projectKey.value, appId.value, 'disclosure', lang, DocumentDownloadVersion.None)
+  ProjectService.downloadDocumentByTask(
+    currentProject.value._key,
+    appId.value,
+    'disclosure',
+    lang,
+    DocumentDownloadVersion.None,
+  )
     .then((res) => {
-      link.download = `Disclosure_${projectName.value.replaceAll(' ', '_')}-${lang}.pdf`;
+      link.download = `Disclosure_${currentProject.value.name.replaceAll(' ', '_')}-${lang}.pdf`;
       link.href = URL.createObjectURL(new Blob([res.data as BlobPart]));
       link.click();
     })
@@ -53,46 +57,25 @@ defineExpose({open});
 </script>
 
 <template>
-  <v-form ref="form">
-    <v-dialog v-model="show" content-class="large" scrollable width="700" persistent>
-      <v-card class="pa-8 dDialog">
-        <v-card-title>
-          <v-row>
-            <v-col cols="10">
-              <span class="text-h5">{{ t('DOWNLOAD_DOCUMENT') }}</span>
-            </v-col>
-            <v-col cols="2" align="right">
-              <DCloseButton @click="close" />
-            </v-col>
-          </v-row>
-        </v-card-title>
-        <v-card-text class="pt-2">
-          <v-row class="py-2">
-            <!-- v-html is used to render because DOWNLOAD_INTITIAL_DOCUMENT contains html -->
-            <v-col cols="12" xs="12" class="pb-10" v-html="t('DOWNLOAD_INITIAL_DOCUMENT')"> </v-col>
-            <v-col xs="6">
-              <DCActionButton
-                text="Download (de)"
-                icon="mdi-download"
-                :hint="t('TT_TAD_disco_doc')"
-                @click="downloadDiscoDocument('de')"
-                class="none-uppercase" />
-            </v-col>
-            <v-col xs="6">
-              <DCActionButton
-                text="Download (en)"
-                icon="mdi-download"
-                :hint="t('TT_TAD_disco_doc')"
-                @click="downloadDiscoDocument('en')"
-                class="none-uppercase" />
-            </v-col>
-          </v-row>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <DCActionButton isDialogButton @click="close" :text="t('BTN_OK')" variant="elevated"> </DCActionButton>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
-  </v-form>
+  <v-dialog v-model="show" content-class="large" scrollable width="700" persistent>
+    <DialogLayout :config="{title: t('DOWNLOAD_DOCUMENT')}" @close="close">
+      <Stack>
+        <div v-html="t('DOWNLOAD_INITIAL_DOCUMENT')"></div>
+        <Stack direction="row">
+          <DCActionButton
+            text="Download (de)"
+            icon="mdi-download"
+            :hint="t('TT_TAD_disco_doc')"
+            @click="downloadDiscoDocument('de')"
+            class="none-uppercase" />
+          <DCActionButton
+            text="Download (en)"
+            icon="mdi-download"
+            :hint="t('TT_TAD_disco_doc')"
+            @click="downloadDiscoDocument('en')"
+            class="none-uppercase" />
+        </Stack>
+      </Stack>
+    </DialogLayout>
+  </v-dialog>
 </template>
